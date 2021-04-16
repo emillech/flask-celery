385ae8f0daf97b810d22013500d1ad4b091a2a51
Emil Lech <emil.lech@Mac-mini-Emil-Lech.local>
2021-04-16 11:44:53 +0200

    conf ok

--- tasks.py
+++ tasks.py
@@ -2,14 +2,21 @@ from flask import Flask
 from flask_celery_conf import make_celery
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'amqb://localhost//'
+app.config['CELERY_BROKER_URL'] = 'amqp://localhost//'
 app.config['CELERY_RESULT_BACKEND'] = 'db+sqlite:///db.sqlite3'
 
 celery = make_celery(app)
 
+
 @app.route('/process/<name>')
 def process(name):
-    return name
+    reverse.delay(name)
+    return 'I sent request'
+
+
+@celery.task(name='celery_example.reverse')
+def reverse(text):
+    return text[::-1]
 
 
 if __name__ == '__main__':
